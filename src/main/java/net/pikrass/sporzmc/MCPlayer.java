47248491e1e53da2ec730879d5fd36793ba762bf
Pikrass <contact@pikrass.net>
2015-08-01 18:39:20 +0200

    Implement notification of rounds and attributions

--- src/main/java/net/pikrass/sporzmc/MCPlayer.java
+++ src/main/java/net/pikrass/sporzmc/MCPlayer.java
@@ -1,19 +1,94 @@
 package net.pikrass.sporzmc;
 
+import static net.pikrass.sporzmc.util.I18n.*;
+import static net.pikrass.sporzmc.util.MinecraftHelper.green;
+
 import net.pikrass.sporz.*;
 import net.pikrass.sporz.events.*;
 import net.pikrass.sporz.actions.*;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.management.ServerConfigurationManager;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayerMP;
 
 public class MCPlayer extends Player {
 	public MCPlayer(String username) {
 		super(username);
 	}
 
+	protected Entity getEntity() {
+		ServerConfigurationManager manager =
+			MinecraftServer.getServer().getConfigurationManager();
+		return manager.getPlayerByUsername(getName());
+	}
+
+	protected void sendMsg(String message) {
+		Entity entity = getEntity();
+		if(entity == null)
+			return;
+
+		net.pikrass.sporzmc.util.MinecraftHelper.sendMsg(entity, message);
+	}
+
 	public void notifyRound(int num, RoundPeriod period) {
-		//TODO
+		if(period == RoundPeriod.DAY)
+			sendMsg(String.format(_("========= DAY %d ========="), num));
+		else
+			sendMsg(String.format(_("======== NIGHT %d ========"), num));
 	}
 	public void notify(Attribution event) {
-		//TODO
+		switch(event.getRole()) {
+		case ASTRONAUT:
+			if(event.getState() == State.HUMAN) {
+				sendMsg(green(_("You are an astronaut")));
+				sendMsg(_("You don't have any particular power"));
+			} else {
+				sendMsg(green(_("You are a mutant")));
+				sendMsg(_("You don't have any particular power, but you can't be healed"));
+			}
+			break;
+		case DOCTOR:
+			sendMsg(green(_("You are a doctor")));
+			sendMsg(_("Each night, if you're not a mutant, you can heal or kill players"));
+
+			StringBuffer names = new StringBuffer();
+			int nb = event.getGroup().size();
+			for(Player p : event.getGroup()) {
+				if(!p.equals(this))
+					names.append(p.getName()+", ");
+			}
+			if(nb > 0) {
+				String group = names.substring(0, names.length()-2);
+				sendMsg(String.format(_("The other doctor is %s", "The other doctors are %s", nb), group));
+			}
+
+			break;
+		case PSYCHOLOGIST:
+			sendMsg(green(_("You are a psychologist")));
+			sendMsg(_("Each night you can see the state of another player (human or mutant)"));
+			break;
+		case GENETICIST:
+			sendMsg(green(_("You are a geneticist")));
+			sendMsg(_("Each night you can see the genome of another player (standard, host or resistant)"));
+			break;
+		case COMPUTER_ENGINEER:
+			sendMsg(green(_("You are a computer engineer")));
+			sendMsg(_("Each night you can see the number of mutants on board"));
+			break;
+		case HACKER:
+			sendMsg(green(_("You are a hacker")));
+			sendMsg(_("Each night you can see what a psychologist, geneticist or computer engineer saw"));
+			break;
+		case SPY:
+			sendMsg(green(_("You are a spy")));
+			sendMsg(_("Each night you can see what happened to another player"));
+			break;
+		case TRAITOR:
+			sendMsg(green(_("You are a traitor")));
+			sendMsg(_("Your goal is always to make the mutants win, regardless of your own state"));
+			break;
+		}
 	}
 	public void notify(NewCaptain event) {
 		//TODO
--- src/main/java/net/pikrass/sporzmc/PigPlayer.java
+++ src/main/java/net/pikrass/sporzmc/PigPlayer.java
@@ -4,7 +4,11 @@ import net.pikrass.sporz.*;
 import net.pikrass.sporz.events.*;
 import net.pikrass.sporz.actions.*;
 
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.management.ServerConfigurationManager;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.EntityPig;
+import net.minecraft.util.ChatComponentText;
 
 public class PigPlayer extends MCPlayer {
 	private EntityPig entity;
@@ -12,5 +16,17 @@ public class PigPlayer extends MCPlayer {
 	public PigPlayer(String username, EntityPig pig) {
 		super(username);
 		this.entity = pig;
+	}
+
+	protected Entity getEntity() {
+		return entity;
+	}
+
+	protected void sendMsg(String message) {
+		message = "<"+getName()+"> " + message;
+
+		ServerConfigurationManager manager =
+			MinecraftServer.getServer().getConfigurationManager();
+		manager.sendChatMsg(new ChatComponentText(message));
 	}
 }
